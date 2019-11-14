d9299bd97e62c76a6f4ff2b00ff62c86c2d27698
Gregor Best <gbe@unobtanium.de>
2019-11-14 23:16:12 +0100

    Add notifications for subscribed people

--- proxy.go
+++ proxy.go
@@ -12,6 +12,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"sync"
 	"time"
 
 	"golang.org/x/net/websocket"
@@ -27,20 +28,27 @@ const (
 )
 
 type Proxy struct {
+	sync.Mutex
 	// Maps URLs to active messages, keyed by message ID
 	activeAlerts map[URL]map[MessageID]alertMessage
+	updateChans  map[chan bool]bool
 }
 
 func newProxy() Proxy {
 	return Proxy{
 		activeAlerts: make(map[URL]map[MessageID]alertMessage),
+		updateChans:  make(map[chan bool]bool),
 	}
 }
 
 // getMatchingAlerts returns all alerts that have areas affecting the provided coordinate
 // This function is really fucking ugly.
-func (p Proxy) getMatchingAlerts(c Coordinate) []alertMessage {
-	var alerts []alertMessage
+func (p *Proxy) getMatchingAlerts(c Coordinate) []alertMessage {
+	p.Lock()
+	defer p.Unlock()
+
+	// Create empty list. This doesn't use the `nil` pattern for new slices because those encode to `null` values in JSON.
+	alerts := make([]alertMessage, 0)
 
 loop:
 	for _, alert := range p.activeAlerts {
@@ -66,16 +74,36 @@ loop:
 	return alerts
 }
 
+func (p *Proxy) registerUpdateChan(ch chan bool) {
+	p.Lock()
+	defer p.Unlock()
+
+	p.updateChans[ch] = true
+}
+
+func (p *Proxy) unregisterUpdateChan(ch chan bool) {
+	p.Lock()
+	defer p.Unlock()
+
+	delete(p.updateChans, ch)
+}
+
 // socketHandler runs a client connection
-func (p Proxy) socketHandler(conn *websocket.Conn) {
+func (p *Proxy) socketHandler(conn *websocket.Conn) {
 	// When a user sends a Lat/Lon pair, check active alerts on p
 	// Otherwise, watch the active alerts for new things
 
+	updateChan := make(chan bool)
 	coords := make(chan Coordinate)
 	quit := make(chan interface{})
 
+	p.registerUpdateChan(updateChan)
+	defer p.unregisterUpdateChan(updateChan)
+
 	// First part, run a goroutine to wait changes on the active alerts
 	go func() {
+		var currentCoords Coordinate
+		coordsSet := false // True if coordinates have been received
 		enc := json.NewEncoder(conn)
 
 		log.Println("Waiting for changes in active alerts or coordinates")
@@ -83,9 +111,18 @@ func (p Proxy) socketHandler(conn *websocket.Conn) {
 		for {
 			select {
 			case c := <-coords:
+				currentCoords = c
+				coordsSet = true
 				log.Println("Received new coordinate:", c)
 				alerts := p.getMatchingAlerts(c)
 				enc.Encode(&alerts)
+			case <-updateChan:
+				if coordsSet {
+					alerts := p.getMatchingAlerts(currentCoords)
+					enc.Encode(&alerts)
+				} else {
+					log.Println("not checking update, no coords set")
+				}
 			case <-quit:
 				break loop
 			}
@@ -113,33 +150,39 @@ func (p Proxy) socketHandler(conn *websocket.Conn) {
 
 // socketHandshake is a dumb handshake handler for the websocket. This is required because the websocket library checks the origin
 // by default and rejects requests with an unexpected origin.
-func (p Proxy) socketHandshake(conf *websocket.Config, req *http.Request) error {
+func (p *Proxy) socketHandshake(conf *websocket.Config, req *http.Request) error {
 	log.Println("ws handshake. conf:", conf, "req:", req)
 	return nil
 }
 
-func (p Proxy) updateData(url URL) error {
+// updateData requests new data from url and updates the stored alert messages. It returns true if an update was performed, and
+// false if no new data arrived
+func (p *Proxy) updateData(url URL) (bool, error) {
+	p.Lock()
+	defer p.Unlock()
+
 	if _, ok := p.activeAlerts[url]; !ok {
 		p.activeAlerts[url] = make(map[MessageID]alertMessage)
 	}
 
 	resp, err := http.Get(string(url))
 	if err != nil {
-		return err
+		return false, err
 	}
 	defer resp.Body.Close()
 
 	content, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return err
+		return false, err
 	}
 
 	var alerts []alertMessage
 	err = json.Unmarshal(content, &alerts)
 	if err != nil {
-		return err
+		return false, err
 	}
 
+	// Track new alerts
 	newAlerts := make([]alertMessage, 0)
 
 	for _, alert := range alerts {
@@ -150,37 +193,41 @@ func (p Proxy) updateData(url URL) error {
 		}
 	}
 
-	// TODO: Handle new messages
-	for _, alerts := range newAlerts {
-		p.handleNewMessage(alerts)
-	}
-
 	// Set new active message map
 	p.activeAlerts[url] = make(map[MessageID]alertMessage)
 	for _, message := range alerts {
 		p.activeAlerts[url][message.Identifier] = message
 	}
 
-	return nil
+	return len(newAlerts) != 0, nil
 }
 
-func (p Proxy) handleNewMessage(alert alertMessage) {
-	log.Println("Would handle new message", alert.Identifier, "now")
-}
-
-func (p Proxy) run() {
+func (p *Proxy) run() {
 	// Periodically poll all URLs and update proxy state. On update, check subscribed customers for area containment and notify them.
 	urls := []URL{url1, url2, url3, url4}
 
-	ticker := time.NewTicker(60 * time.Second)
+	ticker := time.NewTicker(1 * time.Minute)
 
 	for range ticker.C {
+		needUpdate := false
 		for _, url := range urls {
-			err := p.updateData(url)
+			newData, err := p.updateData(url)
 			if err != nil {
-				log.Fatalln("updating", url, "failed:", err)
+				log.Println("updating", url, "failed:", err)
+				continue
 			}
 			log.Println(url, "refreshed")
+			needUpdate = needUpdate || newData
+		}
+		if needUpdate {
+			log.Println("Notifying connected clients of updates")
+			// Non-blocking notify to make sure slow clients don't block us
+			for ch := range p.updateChans {
+				select {
+				case ch <- true:
+				default:
+				}
+			}
 		}
 		log.Println("waiting 60 seconds for next update")
 	}
