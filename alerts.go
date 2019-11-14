b715ffef4ccdda3343a9988a1994862d2042c346
Gregor Best <gbe@unobtanium.de>
2019-11-14 22:02:08 +0100

    First draft of proxy code
    websockets are still missing

---
+++ alerts.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type URL string
+type MessageID string
+
+type geocodeDescription struct {
+	ValueName string `json:"valueName"`
+	Value     string `json:"value"`
+}
+
+type areaDescription struct {
+	Description string               `json:"areaDesc"`
+	Polygon     []string             `json:"polygon"` // TODO: Add proper extraction
+	Geocode     []geocodeDescription `json:"geocode"`
+}
+
+type infoItem struct {
+	Language           string            `json:"language"`
+	Category           []string          `json:"category"`     // e.g. []{"Safety"} or []{"Met"} for meterological messages
+	Event              string            `json:"event"`        // e.g. "Gefahrenmitteilung", "Gefahreninformation", sometimes just a code
+	ResponseType       []string          `json:"responseType"` // suggested response e.g. []{"Monitor", "Prepare"}
+	Urgency            string            `json:"urgency"`      // e.g. "Immediate"
+	Severity           string            `json:"severity"`     // e.g. "Minor"
+	Certainty          string            `json:"certainty"`    // How certain is this message? "Observed"
+	Headline           string            `json:"headline"`
+	Description        string            `json:"description"`
+	Instructions       string            `json:"instruction"`
+	ContactInformation string            `json:"contact"`
+	URL                URL               `json:"web"`
+	Area               []areaDescription `json:"area"` // List of affected areas
+	Expires            time.Time         `json:"expires"`
+
+	// TODO: parameter: list of key-value metadata items
+}
+
+func (i infoItem) String() string {
+	return fmt.Sprintf("[Category: %v, Event: %s, ResponseType: %v, Urgency: %s, Severity: %s, Headline: %s, Description: %s, Instructions: %s, Contact: %s, URL: %s, Expires: %s]",
+		i.Category, i.Event, i.ResponseType, i.Urgency, i.Severity, i.Headline, i.Description, i.Instructions, i.ContactInformation, i.URL, i.Expires)
+}
+
+type alertMessage struct {
+	Identifier MessageID `json:"identifier"`
+	Sender     string    `json:"sender"`
+	Sent       time.Time `json:"sent"`    // Timestamp
+	Status     string    `json:"status"`  // Is this a current message?
+	MsgType    string    `json:"msgType"` // "Cancel", "Alert"
+	Scope      string    `json:"scope"`   // e.g. "Public"
+
+	// `code` is ignored here
+	Info []infoItem `json:"info"`
+}
+
+func (m alertMessage) String() string {
+	return fmt.Sprintf("[Identifier: %s, Sender: %s, Sent: %s, Status: %s, MsgType: %s, Scope: %s, Info: %s]",
+		m.Identifier, m.Sender, m.Sent, m.Status, m.MsgType, m.Scope, m.Info)
+}
--- proxy.go
+++ proxy.go
@@ -1,7 +1,6 @@
 package main
 
 // TODO: The big ticket items missing are:
-// - Handle area information, extract lat/lon polygon
 // - Expose websocket, allow clients to request data for particular location
 // - Check existing data for matches
 // - Poll for new data, check matches
@@ -13,90 +12,99 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 )
-
-type geocodeDescription struct {
-	ValueName string `json:"valueName"`
-	Value     string `json:"value"`
-}
-
-type areaDescription struct {
-	Description string               `json:"areaDesc"`
-	Polygon     []string             `json:"polygon"` // TODO: Add proper extraction
-	Geocode     []geocodeDescription `json:"geocode"`
-}
-
-type infoItem struct {
-	Language           string            `json:"language"`
-	Category           []string          `json:"category"`     // e.g. []{"Safety"}
-	Event              string            `json:"event"`        // e.g. "Gefahrenmitteilung", "Gefahreninformation", sometimes just a code
-	ResponseType       []string          `json:"responseType"` // e.g. []{"Monitor"}
-	Urgency            string            `json:"urgency"`      // e.g. "Immediate"
-	Severity           string            `json:"severity"`     // e.g. "Minor"
-	Certainty          string            `json:"certainty"`    // How certain is this message? "Observed"
-	Headline           string            `json:"headline"`
-	Description        string            `json:"description"`
-	Instructions       string            `json:"instruction"`
-	ContactInformation string            `json:"contact"`
-	URL                string            `json:"web"`
-	Area               []areaDescription `json:"area"` // List of affected areas
-	Expires            string            `json:"expires"`
-
-	// TODO: parameter: list of key-value metadata items
-}
-
-type messagePayload struct {
-	Identifier string `json:"identifier"`
-	Sender     string `json:"sender"`
-	Sent       string `json:"sent"`   // Timestamp
-	Status     string `json:"status"` // Is this a current message?
-	MsgType    string `json:"msgType"`
-	Scope      string `json:"scope"` // e.g. "Public"
-
-	// `code` is ignored here
-	Info []infoItem `json:"info"`
-}
 
 const url1 = "https://warnung.bund.de/bbk.mowas/gefahrendurchsagen.json"
 const url2 = "https://warnung.bund.de/bbk.biwapp/warnmeldungen.json"
+const url3 = "https://warnung.bund.de/bbk.dwd/unwetter.json"
 
 // TODO: Handle these as well, need to see them in action
-const url3 = "https://warnung.bund.de/bbk.dwd/unwetter.json"
 const url4 = "https://warnung.bund.de/bbk.lhp/hochwassermeldungen.json"
 
-func getData(url string) ([]messagePayload, error) {
-	resp, err := http.Get(url)
+type Proxy struct {
+	// Maps URLs to active messages, keyed by message ID
+	activeAlerts map[URL]map[MessageID]alertMessage
+}
+
+func newProxy() Proxy {
+	return Proxy{
+		activeAlerts: make(map[URL]map[MessageID]alertMessage),
+	}
+}
+
+func (p Proxy) updateData(url URL) error {
+	if _, ok := p.activeAlerts[url]; !ok {
+		p.activeAlerts[url] = make(map[MessageID]alertMessage)
+	}
+
+	resp, err := http.Get(string(url))
 	if err != nil {
-		return nil, err
+		return err
 	}
 	defer resp.Body.Close()
 
 	content, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	var payload []messagePayload
-	err = json.Unmarshal(content, &payload)
+	var alerts []alertMessage
+	err = json.Unmarshal(content, &alerts)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	return payload, err
+	newAlerts := make([]alertMessage, 0)
+
+	for _, alert := range alerts {
+		// Update messages for this url
+		if _, ok := p.activeAlerts[url][alert.Identifier]; !ok {
+			// This message is new, track it.
+			newAlerts = append(newAlerts, alert)
+		}
+	}
+
+	// TODO: Handle new messages
+	for _, alerts := range newAlerts {
+		p.handleNewMessage(alerts)
+	}
+
+	// Set new active message map
+	p.activeAlerts[url] = make(map[MessageID]alertMessage)
+	for _, message := range alerts {
+		p.activeAlerts[url][message.Identifier] = message
+	}
+
+	return nil
+}
+
+func (p Proxy) handleNewMessage(alert alertMessage) {
+	log.Println("Would handle new message", alert.Identifier, "now")
+}
+
+func (p Proxy) run() {
+	// Periodically poll all URLs and update proxy state. On update, check subscribed customers for area containment and notify them.
+	urls := []URL{url1, url2, url3, url4}
+
+	ticker := time.NewTicker(60 * time.Second)
+
+	for range ticker.C {
+		for _, url := range urls {
+			err := p.updateData(url)
+			if err != nil {
+				log.Fatalln("updating", url, "failed:", err)
+			}
+			log.Println(url, "refreshed")
+		}
+		log.Println("waiting 60 seconds for next update")
+	}
 }
 
 func main() {
 	log.Println("Here we go")
 
-	p, err := getData(url1)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	log.Println(url1, p)
+	proxy := newProxy()
 
-	p, err = getData(url2)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	log.Println(url2, p)
+	proxy.run()
 }
